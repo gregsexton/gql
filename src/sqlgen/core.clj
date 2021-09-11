f3304cdc11f22d76ff71db6d4ecd0ef478ff89a3
Ubuntu <ubuntu@ip-172-31-40-92.eu-west-2.compute.internal>
2021-09-11 21:29:00 +0100

    Precedence

--- src/sqlgen/core.clj
+++ src/sqlgen/core.clj
@@ -4,6 +4,26 @@
             [clojure.walk :as walk])
   (:gen-class))
 
+(defn get-selection-cols [query]
+  (->> (query :select)
+       (map (fn [col] (if (vector? col)
+                        (second col)
+                        col)))))
+
+(defn precedence-merge [query fragment]
+  (let [precedence (zipmap [:from :where :group-by :having :select :order-by :limit] (range))]
+    (letfn [(prec [a]
+              (if (empty? a)
+                0
+                (->> (keys a)
+                     (map precedence)
+                     (apply max))))
+            (higher-precedence [a b]
+              (<= (prec a) (prec b)))]
+      (if (higher-precedence fragment query)
+        (merge {:select (get-selection-cols query) :from query} fragment)
+        (merge query fragment)))))
+
 (defn keywordize [form] (if (symbol? form) (keyword form) form))
 
 (defn keyword-or-alias [form]
@@ -21,6 +41,7 @@
                 (~'or [& args#] `[:or ~@(map keywordize args#)])
                 (~'= [a# b#] `[:= ~(keywordize a#) ~(keywordize b#)])
                 (~'not= [a# b#] `[:<> ~(keywordize a#) ~(keywordize b#)])
+                (~'not [a#] `[:not ~(keywordize a#)])
                 (~'json-extract-scalar [from# path#]
                  `[[:json_extract_scalar ~(keywordize from#) ~path#]])]
                ~expr))
@@ -29,14 +50,17 @@
   `{:from [~(keyword tbl)]})
 
 (defmacro select [ds & exprs]
-  `(merge ~ds {:select [~@(map keyword-or-alias exprs)]}))
+  `(precedence-merge ~ds {:select [~@(map keyword-or-alias exprs)]}))
 
 (defmacro where [ds & exprs]
-  `(merge ~ds {:where (expand-expr (~'and ~@exprs))}))
+  `(precedence-merge ~ds {:where (expand-expr (~'and ~@exprs))}))
 
 ;;; allowing referencing just declared vars by substituting. what's
 ;;; the tradeoff compared to nesting? is an engine smart enough not to
 ;;; duplicate effort?
+;;; nesting might prevent effort dup and it also takes care of using
+;;; multipe mutates and referring to vars, there might be a _lot_ of
+;;; nesting though.
 (defmacro mutate [ds & forms]
   (let [pairs (partition 2 forms)
         update-form `(update-in ~ds [:select] concat
@@ -44,11 +68,9 @@
                                                    ~(mapv named-expr pairs)))]
     `(expand-expr ~(m/mexpand-all update-form))))
 
-;;; TODO: precedence - you usually can't refer to things you've added to select, so will have to be careful around mutate
 ;;; TODO: grouping
 ;;; TODO: joins
 ;;; TODO: allow using variables - probably need to use binding?
-
 (-> (table src_wa_fastdesk_tickets)
     (where (= ds "<DATEID>")
            (or flag-col
@@ -56,9 +78,11 @@
                   "backup"))
            (not= (json-extract-scalar data "$.status") nil))
     (select data (id :as ticket-id))
+    (where (= id "1234"))               ;this demonstrates precedence
     (mutate topic (json-extract-scalar data "$.topic")
             status (json-extract-scalar data "$.status")
             is-closed (= status "closed")) ;note: referring to status
+    (mutate not-is-closed (not is-closed))  ;TODO: this does not work currently
     (sql/format :inline true))
 
 ;;; how to represent the table metadata and pass that along?
