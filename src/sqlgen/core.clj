ecf618bad79c7cb8a0b4d471d751353f21ef30fc
Ubuntu <ubuntu@ip-172-31-40-92.eu-west-2.compute.internal>
2021-09-11 20:41:35 +0100

    Mutate

--- src/sqlgen/core.clj
+++ src/sqlgen/core.clj
@@ -4,25 +4,68 @@
             [clojure.walk :as walk])
   (:gen-class))
 
-(defmacro select [ds & exprs]
-  `(merge ~ds {:select [~@(map keyword exprs)]}))
-
 (defn keywordize [form] (if (symbol? form) (keyword form) form))
+
+(defn keyword-or-alias [form]
+  (cond (symbol? form) (keyword form)
+        (list? form) [(keyword (first form)) (keyword (nth form 2))]  ;:as should be 2nd
+        :else form))
+
+(defn named-expr [pair]
+  (let [col (first pair)
+        expr (second pair)]
+    [expr (keyword col)]))
 
 (defmacro expand-expr [expr]
   `(m/macrolet [(~'and [& args#] `[:and ~@(map keywordize args#)])
-                (~'or [& args#] `[:and ~@(map keywordize args#)])
+                (~'or [& args#] `[:or ~@(map keywordize args#)])
                 (~'= [a# b#] `[:= ~(keywordize a#) ~(keywordize b#)])
+                (~'not= [a# b#] `[:<> ~(keywordize a#) ~(keywordize b#)])
                 (~'json-extract-scalar [from# path#]
                  `[[:json_extract_scalar ~(keywordize from#) ~path#]])]
                ~expr))
 
-(defmacro where [ds expr]
-  `(merge ~ds {:where (expand-expr ~expr)}))
+(defmacro table [tbl]
+  `{:from [~(keyword tbl)]})
 
-;;; i can't have keyword symbols eval expand-expr otherwise the symbols will be evaluated. or I use quoting but macrolet is smart enough to not expand that
-;;; i can't have expand-expr evaluate keyword symbols because all the symbols I'm hoping to expand will be keywordised
-;;; I have to have expand-expr as it is walking down keywordise specific things, which is probably the safer thing to do anyway
+(defmacro select [ds & exprs]
+  `(merge ~ds {:select [~@(map keyword-or-alias exprs)]}))
+
+(defmacro where [ds & exprs]
+  `(merge ~ds {:where (expand-expr (~'and ~@exprs))}))
+
+;;; allowing referencing just declared vars by substituting. what's
+;;; the tradeoff compared to nesting? is an engine smart enough not to
+;;; duplicate effort?
+(defmacro mutate [ds & forms]
+  (let [pairs (partition 2 forms)
+        update-form `(update-in ~ds [:select] concat
+                                (m/symbol-macrolet [~@forms]
+                                                   ~(mapv named-expr pairs)))]
+    `(expand-expr ~(m/mexpand-all update-form))))
+
+;;; TODO: precedence - you usually can't refer to things you've added to select, so will have to be careful around mutate
+;;; TODO: grouping
+;;; TODO: joins
+;;; TODO: allow using variables - probably need to use binding?
+
+(-> (table src_wa_fastdesk_tickets)
+    (where (= ds "<DATEID>")
+           (or flag-col
+               (= (json-extract-scalar data "$.topic")
+                  "backup"))
+           (not= (json-extract-scalar data "$.status") nil))
+    (select data (id :as ticket-id))
+    (mutate topic (json-extract-scalar data "$.topic")
+            status (json-extract-scalar data "$.status")
+            is-closed (= status "closed")) ;note: referring to status
+    (sql/format :inline true))
+
+;;; how to represent the table metadata and pass that along?
+
+(mutate {:select []}
+        status (json-extract-scalar data "$.status")
+        is-closed (= status "closed"))
 
 (expand-expr (json-extract-scalar 5 4))
 (expand-expr (json-extract-scalar foo 4))
@@ -32,16 +75,3 @@
              "backup"))
 (where {} (= (json-extract-scalar data "$.topic")
              another-col))
-
-(defmacro table [tbl]
-  `{:from [~(keyword tbl)]})
-
-(-> (table src_wa_fastdesk_tickets)
-    (select data)
-    (where (and (= ds "<DATEID>")
-                flag_col
-                (= (json-extract-scalar data "$.topic")
-                   "backup")))
-    (sql/format))
-
-;;; how to represent the table metadata and pass that along?
